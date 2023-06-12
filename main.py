19d62eb30a868adfa47b742456116434def3b266
dim-sDev <dimitriaigle@gmail.com>
2023-06-12 11:43:47 -0400

    fichier avec que les huricanes nommé

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 import pandas as pd
 import os
 
+name_huricane  = '';
+data = '';
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    dataByCountry = pd.read_csv("data_1686321075.csv")
-    dataLatinAmericaAndCaraibes = pd.read_csv("data_1686320929.csv")
-    unique_countries = dataByCountry['Country__ESTANDAR'].unique()
+def getNamesHuricanes():
+    df = pd.read_csv("Historical_Hurricane_Tracks.csv")
+    unique_hurricanes = df["NAME"].unique()
+    for name_hurricane in unique_hurricanes:
+        print(name_hurricane)
 
+    df = df[df["NAME"] != "NOT_NAMED"]
+    print(df)
+    df.to_csv("Hurricane_Name.csv")
+
+def setCsvByCountry():
+    dataByCountry = pd.read_csv("data_1686321075.csv")
+    #dataLatinAmericaAndCaraibes = pd.read_csv("data_1686320929.csv")
+    unique_countries = dataByCountry['Country__ESTANDAR'].unique()
     # Créer le dossier 'Country' s'il n'existe pas déjà
     folder_name = 'Country'
     if not os.path.exists(folder_name):
@@ -20,6 +31,5 @@ if __name__ == '__main__':
         filename = f'{folder_name}/{country}.csv'
         filtered_df.to_csv(filename, index=False)
 
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+        getNamesHuricanes()

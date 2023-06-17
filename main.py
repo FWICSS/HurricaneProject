4b12f60b11b33f4d0d5f8541d878406388ece227
dim-sDev <dimitriaigle@gmail.com>
2023-06-17 16:15:33 -0400

    Génération du Dossier Hurricane chaque ouragan a son fichier dossier data contient tout les fichier csv

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ name_huricane  = '';
 data = '';
 # Press the green button in the gutter to run the script.
 def getNamesHuricanes():
-    df = pd.read_csv("Historical_Hurricane_Tracks.csv")
+    df = pd.read_csv("data/Historical_Hurricane_Tracks.csv")
     unique_hurricanes = df["NAME"].unique()
     for name_hurricane in unique_hurricanes:
         print(name_hurricane)
@@ -16,7 +16,7 @@ def getNamesHuricanes():
     df.to_csv("Hurricane_Name.csv")
 
 def setCsvByHurricanes():
-    dataHurricanes = pd.read_csv("Hurricane_Name.csv")
+    dataHurricanes = pd.read_csv("data/Hurricane_Name.csv")
     unique_Hurricanes = dataHurricanes["NAME"].unique()
     folder_name = 'Hurricanes'
 
@@ -24,17 +24,17 @@ def setCsvByHurricanes():
         os.makedirs(folder_name)
     for hurricanes in unique_Hurricanes:
         filter_Hurricanes = dataHurricanes[dataHurricanes["NAME"] == hurricanes]
-
-        filename = f'{folder_name}/{hurricanes}.csv'
+        hurricane = hurricanes.replace(":", "-")
+        filename = f'{folder_name}/{hurricane}.csv'
         filter_Hurricanes.to_csv(filename, index=False)
 
 def listOFBassin():
-    dataHurricanes = pd.read_csv("Hurricane_Name.csv")
+    dataHurricanes = pd.read_csv("data/Hurricane_Name.csv")
     unique_Bassin = dataHurricanes["BASIN"].unique()
     print(f"\n list des basins : {unique_Bassin}")
 
 def setCsvByCountry():
-    dataByCountry = pd.read_csv("data_1686321075.csv")
+    dataByCountry = pd.read_csv("data/data_1686321075.csv")
     #dataLatinAmericaAndCaraibes = pd.read_csv("data_1686320929.csv")
     unique_countries = dataByCountry['Country__ESTANDAR'].unique()
     # Créer le dossier 'Country' s'il n'existe pas déjà
@@ -58,6 +58,5 @@ if __name__ == '__main__':
         4. Compare les trajectoire
         5. Analyser les trajectoire par année
         """)
-        listOFBassin()
-
+        setCsvByHurricanes()
 #%%
